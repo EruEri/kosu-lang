42be9a4677e6921cafb52eff5415b73265085676
EruEri <ndiayeyves@gmail.com>
2022-11-24 19:23:02 +0100

    Remove underscore from used var

--- bin/kosuc.ml
+++ bin/kosuc.ml
@@ -34,11 +34,11 @@ let () =
     Clap.flag ~set_short:'c' ~description:"Produce an object file" false
   in
 
-  let _files = Clap.list_string ~description:"files" ~placeholder:"FILES" () in
+  let files = Clap.list_string ~description:"files" ~placeholder:"FILES" () in
 
   Clap.close ();
 
-  let modules_opt = Cli.files_to_ast_program _files in
+  let modules_opt = Cli.files_to_ast_program files in
 
   match modules_opt with
   | Error e -> (
