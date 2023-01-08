1c371f68f06655fde189d9caaba7239a5a02809e
EruEri <ndiayeyves@gmail.com>
2023-01-08 19:10:20 +0100

    [Asm impl]: Modulo

--- lib/backend/aarch64/aarch64Conv.ml
+++ lib/backend/aarch64/aarch64Conv.ml
@@ -559,6 +559,28 @@ module Codegen = struct
             copy_from_reg r9 waddress rval_rktype rprogram
           ) |> Option.value ~default:[] in 
           r9, linstructions @ rinstructions @ div_instruction::copy_instruction
+      | RVBuiltinBinop {binop = TacSelf TacModulo; blhs; brhs} ->
+        let r9 = tmpreg_of_ktype_2 rprogram blhs.expr_rktype in
+        let r10 = tmpreg_of_ktype_3 rprogram blhs.expr_rktype in
+        let r11 = tmpreg_of_ktype_4 rprogram brhs.expr_rktype in
+        let left_reg, linstructions = translate_tac_expression ~str_lit_map ~target_reg:r10 rprogram fd blhs in
+        let right_reg, rinstructions = translate_tac_expression ~str_lit_map ~target_reg:r11 rprogram fd brhs in
+        let div_instruction = 
+          if KosuIrTyped.Asttyhelper.RType.is_unsigned_integer rval_rktype 
+            then Instruction (UDIV {destination = r9; operand1 = left_reg; operand2 = right_reg}) 
+          else Instruction (UDIV {destination = r9; operand1 = left_reg; operand2 = right_reg}) in
+        let modulo_instruction = [
+                  (* udiv x2, x0, x1 // Thank you StackOverflow
+                msub x3, x2, x1, x0 *)
+          div_instruction;
+          Instruction (MSUB {destination = r9; operand1_base = r9; operand2 = right_reg; scale = left_reg})
+        ] in
+        let copy_instruction = where |> Option.map (fun waddress -> 
+          copy_from_reg r9 waddress rval_rktype rprogram
+        ) |> Option.value ~default:[] in 
+        r9, linstructions @ rinstructions @ modulo_instruction @ copy_instruction
+        
+
       | _ -> failwith "Mostly binop"
 
 let rec translate_tac_statement ~str_lit_map current_module rprogram (fd: FrameManager.frame_desc) = function
