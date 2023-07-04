5283d88c6f7855ee382f9f976695877e004c5a8d
EruEri <ndiayeyves@gmail.com>
2023-07-04 14:57:00 +0200

    [Details]: comment error print stack function

--- bin/kosu.ml
+++ bin/kosu.ml
@@ -15,7 +15,7 @@
 (*                                                                                            *)
 (**********************************************************************************************)
 
-let () = Printexc.record_backtrace true
-let () = Printexc.print_backtrace stderr
+(* let () = Printexc.record_backtrace true
+   let () = Printexc.print_backtrace stderr *)
 let code = KosuClis.Kosu.Cli.eval ()
 let () = exit code
