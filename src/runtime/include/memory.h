576dca5f5a87ccf45f0bbcb82ccdde7d0b0c30ca
EruEri <ndiayeyves@gmail.com>
2023-10-16 23:00:39 +0200

    C runtime: base memory allocator

---
+++ src/runtime/include/memory.h
@@ -0,0 +1,28 @@
+////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                            //
+// This file is part of Kosu                                                                  //
+// Copyright (C) 2023 Yves Ndiaye                                                             //
+//                                                                                            //
+// Kosu is free software: you can redistribute it and/or modify it under the terms            //
+// of the GNU General Public License as published by the Free Software Foundation,            //
+// either version 3 of the License, or (at your option) any later version.                    //
+//                                                                                            //
+// Kosu is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;          //
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR           //
+// PURPOSE.  See the GNU General Public License for more details.                             //
+// You should have received a copy of the GNU General Public License along with Kosu  .       //
+// If not, see <http://www.gnu.org/licenses/>.                                                //
+//                                                                                            //
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __MEMORY_H__
+#define __MEMORY_H__
+
+
+#include <stddef.h>
+#include <stdint.h>
+
+void* kosu_alloc(uint64_t);
+void* kosu_alloc_array(uint64_t, size_t);
+
+#endif
