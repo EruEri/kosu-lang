608edfa5b5f1f36bfaa1b72709d07c40537a1ad8
nayu <ndiayeyves@gmail.com>
2023-11-05 23:57:02 +0100

    [Install]: back to full makefile

--- src/runtime/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-cc --std=c99 -fPIC -Wall -c src/*.c
-ar rcs libkosu.a *.o
--- src/runtime/version.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-
-cat $1/kosu_lang.opam | grep ^version | awk '{print $2}' | sed 's/\"//g' | tr -d '\t\n'
