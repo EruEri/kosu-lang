3c00c29123b1d1c26701430689898848666275b6
EruEri <ndiayeyves@gmail.com>
2023-11-08 01:19:14 +0100

    [Report]: Refacto into functor

--- lib/kosu_diagnostic/log.ml
+++ lib/kosu_diagnostic/log.ml
@@ -16,7 +16,7 @@
 (**********************************************************************************************)
 
 let ascii_reset = "\u{001B}[0m"
-let ( $ ) str fs s = str @@ fs s
+let ( $ ) f g s = f @@ g s
 
 let sprintf ?color ?(bold = false) ?(underline = false) input =
   let m = Option.is_some color || bold || underline in
@@ -45,8 +45,8 @@ let sprintf ?color ?(bold = false) ?(underline = false) input =
 let same_line (loc : Util.Position.position) =
   loc.start_position.pos_lnum = loc.end_position.pos_lnum
 
-let one_line ?(range = `range) ?(prefix = String.empty) ?color ?(bold = false)
-    ?(underline = false) loc line =
+let report_line ?color ?(range = `range) ?(pline = String.empty) ?bold
+    ?underline loc line =
   let open Util.Position in
   let _start_line, start_column = line_column_of_position loc.start_position in
   let _end_line, end_column = line_column_of_position loc.end_position in
@@ -76,44 +76,33 @@ let one_line ?(range = `range) ?(prefix = String.empty) ?color ?(bold = false)
         let s_end = String.empty in
         (start, line, s_end)
   in
-  Printf.sprintf "%s %s%s%s" prefix start
-    (sprintf ?color ~bold ~underline s)
+  Printf.sprintf "%s %s%s%s" pline start
+    (sprintf ?color ?bold ?underline s)
     s_end
 
-let multiple_line ?(prefix = fun _ -> String.empty) ?color ?(bold = false)
-    ?(underline = false) loc lines =
+let multiple_line ?pline ?color ?(bold = false) ?(underline = false) loc lines =
   let open Util.Position in
   let len = List.length lines in
   List.mapi
     (fun i line ->
-      let prefix = prefix (loc.start_position.pos_lnum + i) in
+      let pline =
+        Option.value ~default:String.empty
+        @@ Option.map (fun f -> f @@ (loc.start_position.pos_lnum + i)) pline
+      in
       let f =
         match i with
         | 0 ->
-            one_line ~range:`start_to_full
+            report_line ~range:`start_to_full
         | n when n = len - 1 ->
-            one_line ~range:`start_to_end
+            report_line ~range:`start_to_end
         | _ ->
-            one_line ~range:`full
+            report_line ~range:`full
       in
-      f ~prefix ?color ~bold ~underline loc line
+      f ~pline ?color ~bold ~underline loc line
     )
     lines
 
-type 'b t =
-  | LogFile :
-      string
-      * 'b
-      * ('b ->
-        (Util.Position.position, Util.Position.position list) Either.t option
-        )
-      -> (string * 'b) t
-  | LogString : string -> string t
-
-let log_file file elt fn = LogFile (file, elt, fn)
-let log_string s = LogString s
-
-let pos_format ?(std = stdout) ?prefix severity loc file =
+let report_file ?severity ?bold ?underline ~std pline loc file =
   let open Util.Position in
   let lines =
     String.split_on_char '\n'
@@ -127,62 +116,19 @@ let pos_format ?(std = stdout) ?prefix severity loc file =
       )
       lines
   in
-  (* let () = Printf.fprintf std "%s" (message (file, loc)) in *)
+  let color = Option.map Severity.color severity in
   let () =
     match same_line loc with
     | true ->
-        let prefix =
-          Option.map (fun f -> f loc.start_position.pos_lnum) prefix
-        in
+        let pline = Option.map (fun f -> f loc.start_position.pos_lnum) pline in
+
         let s =
-          one_line ?prefix ~range:`range ~color:(Severity.color severity)
-            ~underline:true loc (List.hd lines)
+          report_line ?pline ~range:`range ?color ?underline loc
+          @@ List.hd lines
         in
         Printf.fprintf std "%s\n" s
     | false ->
-        let lines =
-          multiple_line ?prefix ~color:(Severity.color severity) ~underline:true
-            loc lines
-        in
+        let lines = multiple_line ?pline ?color ?underline ?bold loc lines in
         List.iter (fun line -> Printf.fprintf std "%s\n" line) lines
   in
   ()
-
-let emit :
-    type a.
-    ?std:out_channel ->
-    ?prefix:(int -> string) ->
-    ?message:(a -> string) ->
-    Severity.t ->
-    a t ->
-    unit =
- fun ?(std = stdout) ?prefix ?message severity string_or_file ->
-  match string_or_file with
-  | LogString string ->
-      let message =
-        match message with None -> fun _ -> String.empty | Some f -> f
-      in
-      let () = Printf.fprintf std "%s" @@ message string in
-      let s = sprintf ~color:(Severity.color severity) ~underline:true string in
-      let () = Printf.fprintf std "%s" s in
-      ()
-  | LogFile (file, elt, fn) ->
-      let arg = (file, elt) in
-      let pos = fn elt in
-      let message =
-        match message with None -> fun _ -> String.empty | Some f -> f
-      in
-      let () = Printf.fprintf std "%s\n" (message arg) in
-      let () =
-        match pos with
-        | None ->
-            ()
-        | Some (Either.Left pos) ->
-            pos_format ~std ?prefix severity pos file
-        | Some (Either.Right posses) ->
-            List.iter
-              (fun pos -> pos_format ~std ?prefix severity pos file)
-              posses
-      in
-
-      ()
---
+++ lib/kosu_diagnostic/report.ml
@@ -0,0 +1,97 @@
+(**********************************************************************************************)
+(*                                                                                            *)
+(* This file is part of Kosu                                                                  *)
+(* Copyright (C) 2023 Yves Ndiaye                                                             *)
+(*                                                                                            *)
+(* Kosu is free software: you can redistribute it and/or modify it under the terms            *)
+(* of the GNU General Public License as published by the Free Software Foundation,            *)
+(* either version 3 of the License, or (at your option) any later version.                    *)
+(*                                                                                            *)
+(* Kosu is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;          *)
+(* without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR           *)
+(* PURPOSE.  See the GNU General Public License for more details.                             *)
+(* You should have received a copy of the GNU General Public License along with Kosu.         *)
+(* If not, see <http://www.gnu.org/licenses/>.                                                *)
+(*                                                                                            *)
+(**********************************************************************************************)
+
+module type S = sig
+  type t
+
+  val loc : t -> Util.Position.position list
+  val prefix : string -> t -> string
+
+  val line : int -> string
+  (**
+      [line n] is a function to be called before the display of the error text.
+      It's usually the current line
+      [n] is the current line
+  *)
+
+  val error : t -> string option
+  val hint : t -> string option
+  val warning : t -> string option
+  val severity : t -> Severity.t option
+end
+
+module Make (TS : S) = struct
+  type _ kind =
+    | LogFile : string * TS.t -> (string * TS.t) kind
+    | LogString : string * Severity.t option -> string kind
+
+  let file f e = LogFile (f, e)
+  let string ?severity s = LogString (s, severity)
+
+  let emit :
+      type a.
+      ?std:out_channel -> ?bold:bool -> ?underline:bool -> a kind -> unit =
+   fun ?(std = stdout) ?bold ?underline -> function
+    | LogString (s, severity) ->
+        let () = Printf.fprintf std "%s" @@ s in
+        let color = Option.map Severity.color severity in
+        let s = Log.sprintf ?color ?bold ?underline s in
+        let () = Printf.fprintf std "%s" s in
+        ()
+    | LogFile (file, e) ->
+        let pos = TS.loc e in
+        let message = TS.prefix file e in
+        let () = Printf.fprintf std "%s\n" message in
+
+        let () =
+          match pos with
+          | [] ->
+              ()
+          | _ :: _ as posses ->
+              let severity = TS.severity e in
+              List.iter
+                (fun pos ->
+                  Log.report_file ?severity ~std (Option.some TS.line) pos file
+                )
+                posses
+        in
+        let () =
+          Option.iter (fun s ->
+              let e = Log.sprintf ~color:Severity.Red ?bold "Error" in
+              Printf.fprintf std "%s: %s\n" e s
+          )
+          @@ TS.error e
+        in
+
+        let () =
+          Option.iter (fun s ->
+              let e = Log.sprintf ~color:Severity.Yellow ?bold "Warning " in
+              Printf.fprintf std "%s: %s" e s
+          )
+          @@ TS.error e
+        in
+
+        let () =
+          Option.iter (fun s ->
+              let e = Log.sprintf ~color:Severity.Blue ?bold "Hint" in
+              Printf.fprintf std "%s: %s\n" e s
+          )
+          @@ TS.error e
+        in
+
+        ()
+end
